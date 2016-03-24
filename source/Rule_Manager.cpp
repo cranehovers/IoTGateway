a6c49875e7a6504b27da4bfa09f76a1f252ffb01
caps.liu@foxmail.com <caps.liu@foxmail.com>
2016-03-24 16:04:41 +0800

    Add RuleManager code that manager the rules object Signed-off-by: caps.liu@foxmail.com <caps.liu@foxmail.com>

--- source/Rule_Manager.cpp
+++ source/Rule_Manager.cpp
@@ -1,4 +1,5 @@
 #include "Rule_Manager.h"
+#include "Rule.h"
 
 RuleManager::RuleManager()
 {
@@ -7,5 +8,29 @@ RuleManager::RuleManager()
 
 RuleManager::~RuleManager()
 {
-    //dtor
+    removeAllRules();
+}
+
+void RuleManager::setRule(Rule *rule)
+{
+    if (rule)
+    {
+        rule_list_.push_back(rule);
+    }
+}
+
+void RuleManager::removeAllRules()
+{
+    std::list<Rule*>::iterator e;
+
+    e = rule_list_.begin();
+
+    for (; e != rule_list_.end(); ++e)
+    {
+        Rule *tmp = (*e);
+
+        // delete the rule object
+        // that have been saved into list
+        delete tmp;
+    }
 }
