31df5529de07597ee4586eb1a2ae48834b8a95c5
caps-liu <caps.liu@foxmail.com>
2016-03-27 13:53:56 +0800

    update code of coap rules object Signed-off-by: caps-liu <caps.liu@foxmail.com>

--- source/CoAP_Rules_Resource.cpp
+++ source/CoAP_Rules_Resource.cpp
@@ -36,5 +36,56 @@ void* CoAPRulesResource::Create()
 
 void CoAPRulesResource::handler_post(CoAPCallback &callback)
 {
-    ACE_DEBUG((LM_DEBUG, "at copa rules resource post method\n"));
+    // get the rule string from payload.
+    std::string payload;
+    bool check_flag = false;
+
+    get_wrapper()->get_payload(callback, payload);
+
+    if (payload.empty())
+    {
+        ACE_DEBUG((LM_DEBUG, "the payload of rules is empty\n"));
+    }
+    else // json formate,
+    {
+        cJSON *result = cJSON_Parse(payload.c_str());
+
+        if (result == 0 )
+        {
+            ACE_DEBUG((LM_DEBUG, "failed to parse json string(%s)\n",payload.c_str()));
+        }
+        else //get when object
+        {
+            cJSON *whenObject = cJSON_GetObjectItem(result, "when");
+
+            if (whenObject != 0) // get command id
+            {
+                cJSON *deviceid = cJSON_GetObjectItem(result, "deviceid");
+
+                if (deviceid != 0)
+                {
+                    if (whenObject->type == cJSON_Number &&
+                        deviceid->type == cJSON_Number)
+                    {
+
+                    }
+                }
+             }
+
+             //TODO: triggger object
+
+             cJSON_Delete(result);
+        }
+    }
+
+    if (!check_flag)
+    {
+        get_wrapper()->bad_request(callback);
+    }
+    else
+    {
+        get_wrapper()->ok_request(callback);
+    }
+
+
 }
