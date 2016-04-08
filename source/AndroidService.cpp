c57c4b8d87fd09626203b498137d379d9abc51ec
caps-liu <caps.liu@foxmail.com>
2016-04-08 15:13:15 +0800

    add andorid proxy service interface code into gateway Signed-off-by: caps-liu <caps.liu@foxmail.com>

---
+++ source/AndroidService.cpp
@@ -0,0 +1,29 @@
+#include "Config.h"
+#include "Gateway.h"
+#include "AndroidService.h"
+
+
+int startService()
+{
+    if ( IoTGateway::instance()->Init() >= 0)
+    {
+        IoTGateway::instance()->Start();
+        IoTGateway::instance()->Stop();
+    }
+    else
+    {
+        ACE_DEBUG((LM_DEBUG,
+                    "Failed to initialize gateway, please check...\n"));
+    }
+
+    return 0;
+}
+
+int stopService()
+{
+    ACE_DEBUG((LM_DEBUG, "Stop!!!!\n"));
+
+    IoTGateway::instance()->Stop();
+
+    return 0;
+}
---
+++ source/AndroidService.h
@@ -0,0 +1,10 @@
+#ifndef __ANDROIDSERVICE_H__
+#define __ANDROIDSERVICE_H__
+
+
+extern "C" {
+
+    extern int startService();
+    extern int stopService();
+}
+#endif // __ANDROIDSERVICE_H__
